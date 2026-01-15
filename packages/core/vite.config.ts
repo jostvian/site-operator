b9953b5c5139fa3bf50e8013f26705bb70d24924
jostvian-agent <jostvian-agent@skandia.com.co>
2026-01-15 16:48:06 -0500

    fix: corregida propiedad sourcemapIgnoreList en vite.config.ts

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,9 +4,7 @@ import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        sourcemaps: {
-            ignoreList: (path) => path.includes('node_modules')
-        }
+        sourcemapIgnoreList: (path) => path.includes('node_modules')
     },
     build: {
         lib: {
